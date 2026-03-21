5bcef65af4c3ca436bf350252525644cfe536660
David Arroyo Menéndez <davidam@gmail.com>
2026-03-21 14:43:52 +0100

    surnames: creation

---
+++ surnames/surnames2json.py
@@ -0,0 +1,22 @@
+
+
+import re
+
+filepath="intersurnames.csv"
+with open(filepath) as fp:
+    for line in fp:
+        if re.match(r"(\w+),(\w+)", str(line)):
+            m4 = re.match(r"(\w+),(\w+)", str(line))
+            json_content = '[{\n'
+            json_content += '  "name": ' + m4.group(1) + ',\n'
+            json_content += '  "frequency": ' + m4.group(2) + '\n'
+            json_content += '}]\n'
+            print(json_content)
+            json_file = m4.group(1) + '.json'
+            print(json_file)
+            with open(json_file, "w") as jf:
+                jf.writelines(json_content)
+            jf.close()
+print("---------------------------------------------------------------------------")
+fp.close()
+
